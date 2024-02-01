5463afe9c58ab2826ba2dcc453b0728d6de8393e
williamguthier <willg8766@gmail.com>
2024-02-01 12:57:12 -0500

    Made interface changes(feb1)

--- src/polaris-chip.js
+++ src/polaris-chip.js
@@ -29,6 +29,7 @@ export class PolarisChip extends LitElement {
     super();
     // a variable on this object called title
     this.title = 'My boilerplate';
+    this.link = "#";
   }
 
   // CSS styles are scoped JUST to this element. This uses a technology called
@@ -46,7 +47,19 @@ export class PolarisChip extends LitElement {
     */
       :host {
         /* Always make sure that your element has a default way of being displayed */
-        display: block;
+        display: inline-flex;
+      }
+      span {
+        background-color: Black;
+        color: white;
+        font-size: 24px;
+        margin: 8px;
+        padding: 16px;
+      }
+
+      span:hover {
+        background-color: black;
+        border: 10px solid blue;
       }
     `;
   }
@@ -70,7 +83,7 @@ export class PolarisChip extends LitElement {
     // it is going to print the title of the element. The magic of Lit is that
     // when title is changed (even by inspecting the document and hacking the value)
     // it will automatically update what is displayed and do so incredibly quickly
-    return html`<span>${this.title}</span>`;
+    return html`<a href="${this.link}" rel="noopener noreferrer"></a><span>${this.title}</span>`;
   }
 
   // LitElement uses the properties call to do the following:
@@ -82,6 +95,7 @@ export class PolarisChip extends LitElement {
     return {
       // this is a String. Array, Object, Number, Boolean are other valid values here
       title: { type: String },
+      link: { type: String },
     };
   }
 }
