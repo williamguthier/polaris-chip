736c9a0107afc41c13bcaeefb73ca092470c3fb5
williamguthier <willg8766@gmail.com>
2024-02-18 18:04:13 -0500

    buttons

--- src/counter-app.js
+++ src/counter-app.js
@@ -5,54 +5,85 @@ export class CounterApp extends LitElement {
     static get tag() {
         return 'counter-app';
 }
-
 constructor() {
     super();
-    this.counter = "Counter";
-    this.min = "0";
-    this.max = "100";
+    this.counter = 0;
+    this.min = 0;
+    this.max = 100;
     
+}
+
+
     
-  }
   static get styles() {
     return css`
 
     :host {
-        display: inline-flex;
-        
-    #countercontainer {
-       display: inline-flex;
+      display: block;
+      text-align: center;
     }
-    .counter {
-        padding: 16px;
-        background-color: black;
-        border: 8px solid red;
-        border-radius: 16px;
-        margin: 24px;
-        text-align: center;
-        
-       
+
+    #counter {
+      font-size: 64px;
+      font-weight: bold;
+      color: red;
+      background-color: black;
+     
+     
     }
-      
-      }
+
+    #buttons {
+      display: inline-flex;
+      justify-content: center;
+      padding: 10px;
+      gap: 10px;
+    }
+
+    .button {
+      color: white;
+      font-size: 24px;
+      padding: 12px 24px;
+      font-weight: bold;
+      cursor: pointer;
+    }
+
+    #increment {
+        background-color: green;
+    }
+
+    #decrement {
+        background-color: red;
+    }
+
+    
+
+    .button:hover {
+      background-color: red;
+    }
+
+    .button:disabled {
+      background-color: white;
+      cursor: not-allowed;
+    }
+
       `;
   }
 
   render() {
     return html`
-        <div id="countercontainer">
-            <section class="counter">
-
-      </section>
-  </div>
+        <div id="counter">${this.counter}</div>
+        <div id="buttons">
+            <button id="decrement" class="button" ?disabled="${this.min === this.counter}">-</button>
+            <button id="increment" class="button" ?disabled="${this.max === this.counter}">+</button>
+      </div>
     `}
 
     static get properties() {
 
         return {  
-        counter: { type: String},
-        min: { type: String},
-        max: { type: String},
+        counter: { type: Number},
+        min: { type: Number},
+        max: { type: Number},
         }
     }  
     
