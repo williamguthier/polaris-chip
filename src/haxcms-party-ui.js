210a57371736d96da9c3e81ba6bf7b5a987627c1
williamguthier <willg8766@gmail.com>
2024-03-19 13:06:51 -0400

    DDD

---
+++ src/haxcms-party-ui.js
@@ -0,0 +1,14 @@
+import {html, css } from "lit";
+import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
+import "@lrnwebcomponents/rpg-character/rpg-character.js";
+
+
+export class Haxcmsnpm extends DDD {
+ 
+
+  }
+
+
+globalThis.customElements.define(Haxcms-party-ui.tag, Haxcms-party-ui);
+
+
